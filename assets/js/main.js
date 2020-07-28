afee5b072771f712369a73b2766ebc3b833e926f
canoe <ehgus430880@gmail.com>
2020-07-29 00:28:07 +0900

    css working 2020.7.29

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+(function($) {
+    var $header = $('header');
+    var $wrapper = $('#wrapper');
+
+$.fn.add_mainPanel = function(){
+    $this = $(this);
+
+    $('<div class="bg"></div>').appendTo($this)
+}
+
+$wrapper.add_mainPanel();
+
+})(jQuery);
