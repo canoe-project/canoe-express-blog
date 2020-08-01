c8d92ebef274de27114065a18dce438d7fdb5366
canoe <ehgus430880@gmail.com>
2020-08-01 18:42:36 +0900

    apply flex display in css

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,10 @@
 $.fn.add_mainPanel = function(){
     $this = $(this);
 
-    $('<div class="bg"></div>').appendTo($this)
+    // $('<div class="bg"></div>').appendTo($this)
+    ($this).prepend('<div class="bg"></div>')
 }
 
-$wrapper.add_mainPanel();
+// $wrapper.add_mainPanel();
 
 })(jQuery);
