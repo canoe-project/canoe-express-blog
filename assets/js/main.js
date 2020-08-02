c0593c9c5f9b71adb8c7e3e64e03d95cd9d253ca
canoe <ehgus430880@gmail.com>
2020-08-02 23:28:42 +0900

    save

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,15 @@
     var $header = $('header');
     var $wrapper = $('#wrapper');
 
-$.fn.add_mainPanel = function(){
-    $this = $(this);
+// $.fn.add_mainPanel = function(){
+//     $this = $(this);
 
-    // $('<div class="bg"></div>').appendTo($this)
-    ($this).prepend('<div class="bg"></div>')
-}
+//     //$('<div class="bg"></div>').appendTo($this)
+//     ($this).prepend('<div class="bg"></div>')
+// }
 
 // $wrapper.add_mainPanel();
+
+
 
 })(jQuery);
