af8ea94f253e612df72f8de2c2dbfcd5de9356a1
canoe <ehgus430880@gmail.com>
2020-08-05 10:19:03 +0900

    toggle make

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,51 +6,25 @@
         $content = $('#content'),
         $wrapper = $('#wrapper');
 
-// $.fn.toggle = function(){
-//     $(this).on('click', function(){
-//         if($menu.css('display') == 'none'){
-//             $menu.show()
-//             console.log('show')
-//             // $body.closeMenu()
-//             // 
-//         }
-//         else
-//             $menu.hide()
-//     })
-// }
 
-// const $menu = $('.dropdown');
+/*클릭된 태그 확인용*/
+// $(document).on('click',function(e){
+//     console.log(e.target)
+// })
 
-// $(document).mouseup(e => {
-//    if (!$menu.is(e.target) // if the target of the click isn't the container...
-//    && $menu.has(e.target).length === 0) // ... nor a descendant of the container
-//    {
-//      $menu.removeClass('is-active');
-//   }
-//  });
+$(document).on('mouseup', (e)=>{
+    if( !$menu.is(e.target))
+    {
+            $menu.removeClass('is-active');
+    }
+})
 
-// $('.toggle').on('click', () => {
-//   $menu.toggleClass('is-active');
-// });
-
-$.fn.menuToggleClose = () =>{
-    $(this).on('mouseup', (e)=>{
-        if(!$(this).is(e.target) 
-            && $(this).has(e.target).length === 0){
-                $menu.removeClass('is-active');
-            }
-
-    })
-}
-
-$.fn.menuToggleButton = () => {
+$.fn.menuToggleButton = function(){
     $(this).on('click', () =>{
-        $(this).toggleClass('is-active');
+        $menu.toggleClass('is-active');
     })
 }
 
 $menuButton.menuToggleButton()
-$menuContainer.menuToggleClose()
-
 
 })(jQuery);
