15185a1bdb046cac39c0e0c80bf02f84ac94c791
canoe <ehgus430880@gmail.com>
2020-08-06 23:43:21 +0900

    apply express

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var compression = require('compression')
+var topicRouter = require('./routes/topic');
+var indexRouter = require('./routes/index');
+var helmet = require('helmet');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(helmet());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.get('*', function (request, response, next) {
+//     fs.readdir('./assets/Notice', function (error, filelist) {
+//         request.list = filelist;
+//         next();
+//     });
+// });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/topic', topicRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    res.status(404).send('Sorry cant find that!');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 });
 
-module.exports = app;
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
