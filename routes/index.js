bb2c04369a55553219393ad70fc3fae1075a8a28
canoe <ehgus430880@gmail.com>
2020-08-25 14:59:34 +0900

    Modified router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var userModule = require("../userModule.js");
+var path = require("path");
 
 router.get("/", (req, res) => {
-  res.send("hello world");
+  res.sendFile(path.join(__dirname, "../", "/public/main.html"));
 });
-
 router.get("/data", async (req, res) => {
   res.send(await userModule.fileLoad("Notice"));
 });
