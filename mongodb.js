de7d91ec73341eeb9360ada3ae8f71c627df11d9
canoe <ehgus430880@gmail.com>
2020-11-11 15:40:48 +0900

    Apply .gitignore

--- mongodb.js
+++ mongodb.js
@@ -1,56 +1,114 @@
 const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://canoe:2STs9htj84NwkXzg@cluster0.aeq0z.mongodb.net/blog_post?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  poolSize: 2,
-  promiseLibrary: global.Promise,
-  useUnifiedTopology: true,
-});
+var ObjectId = require("mongodb").ObjectID;
+const creatClient = async () => {
+  return new Promise(async (resolve, reject) => {
+    return resolve(
+      new MongoClient(uri, {
+        useUnifiedTopology: true,
+      })
+    );
+  });
+};
 
 const creatPost = async (newListings) => {
-  return await client
-    .connect()
+  return creatClient()
+    .then((client) => {
+      return client.connect();
+    })
     .then(async (client) => {
-      return client.db("blog_post").collection("notice").insertOne(newListings);
+      return client
+        .db("blog_post")
+        .collection("notice")
+        .insertOne(newListings)
+        .then((result) => {
+          client.close();
+          return result;
+        });
     })
     .then((result) => {
       return `${result.insertedCount} new listing(s) created with the following id(s):`;
     });
 };
 
-const readMongo = async () => {
-  let list = [];
-  return await client
-    .connect()
+// const readMongo = async () => {
+//   let list = [];
+//   return await client
+//     .connect()
+//     .then(async (client) => {
+//       return await client.db().admin().listDatabases();
+//     })
+//     .then(async (dblist) => {
+//       console.log(dblist.databases);
+//       dblist.databases.map(async (database) => {
+//         list.push(database.name);
+//       });
+//     })
+//     .then(() => {
+//       return list;
+//     });
+// };
+
+const readCollection = async (dbName) => {
+  return creatClient()
+    .then((client) => {
+      return client.connect();
+    })
     .then(async (client) => {
-      return await client.db().admin().listDatabases();
-    })
-    .then(async (dblist) => {
-      console.log(dblist.databases);
-      dblist.databases.map(async (database) => {
-        list.push(database.name);
-      });
-    })
-    .then(() => {
-      return list;
+      return await client
+        .db(dbName)
+        .listCollections()
+        .toArray()
+        .then((result) => {
+          client.close();
+          return result;
+        });
     });
 };
 
-const readCollection = async (dbName) => {
-  return await client.connect().then(async (client) => {
-    return await client.db(dbName).listCollections().toArray();
-  });
+const readDocument = async (dbName, collection, tilteID) => {
+  return creatClient()
+    .then((client) => {
+      return client.connect();
+    })
+    .then(async (client) => {
+      return await client
+        .db(dbName)
+        .collection(collection)
+        .find(
+          { _id: ObjectId(tilteID) },
+          { projection: { contents: 1, _id: 0 } }
+        )
+        .toArray()
+        .then((result) => {
+          client.close();
+          return result;
+        });
+    });
 };
 
-const readDocument = async (dbName, collection) => {
-  return await client.connect().then(async (client) => {
-    return await client.db(dbName).collection(collection).find().toArray();
-  });
+const readDocumentTitle = async (dbName, collection) => {
+  return creatClient()
+    .then((client) => {
+      return client.connect();
+    })
+    .then(async (client) => {
+      return await client
+        .db(dbName)
+        .collection(collection)
+        .find({}, { projection: { contents: 0 } })
+        .toArray()
+        .then((result) => {
+          client.close();
+          return result;
+        });
+    });
 };
 
 module.exports = {
   creatPost: creatPost,
-  readMongo: readMongo,
   readCollection: readCollection,
   readDocument: readDocument,
+  readDocumentTitle: readDocumentTitle,
 };
--- routes/uploader.js
+++ routes/uploader.js
@@ -8,20 +8,14 @@ router.post("/create", async (req, res) => {
   console.log(await mongodb.creatPost(req.body));
 });
 
-router.post("/documentlist", async (req, res) => {
-  res.send(await mongodb.readDocument("blog_post", req.body.collection));
+router.post("/documenttitle", async (req, res) => {
+  res.send(await mongodb.readDocumentTitle("blog_post", req.body.collection));
 });
 
-router.get("/mongolist", async (req, res) => {
-  res.send(await mongodb.readMongo());
-});
-
-router.get("/collectionlist", async (req, res) => {
-  res.send(await mongodb.readCollection("blog_post"));
-});
-
-router.get("/document", async (req, res) => {
-  await mongodb.readDocument("blog_post", "notice");
+router.post("/document", async (req, res) => {
+  res.send(
+    await mongodb.readDocument("blog_post", req.body.collection, req.body.id)
+  );
 });
 
 module.exports = router;
